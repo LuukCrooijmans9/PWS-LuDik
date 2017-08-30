90af3b24534124919e9ddad70ac5d950839ef027
LuukCrooijmans9 <31418705+LuukCrooijmans9@users.noreply.github.com>
2017-08-30 15:46:39 +0200

    voegt main method toe aan EvoAI.java

--- MainApp/com/LuDik/EvoAI/EvoAI.java
+++ MainApp/com/LuDik/EvoAI/EvoAI.java
@@ -3,6 +3,7 @@ package com.LuDik.EvoAI;
 import java.awt.FlowLayout;
 
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 import com.simplemove.Board;
 import com.simplemove.InfoPanel;
@@ -37,6 +38,17 @@ public class EvoAI extends JFrame {
 		setTitle("EvoAI");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
+	}
+	
+	public void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+
+				new EvoAI().setVisible(true); 
+			}
+		});
 	}
 	
 }
