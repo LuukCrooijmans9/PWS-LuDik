3201fef6df38f91633045dcf080b7fb3ded11ad1
LuukCrooijmans9 <31418705+LuukCrooijmans9@users.noreply.github.com>
2017-08-30 17:10:32 +0200

    Begin aan actionpanel gemaakt

--- MainApp/com/LuDik/EvoAI/ActionPanel.java
+++ MainApp/com/LuDik/EvoAI/ActionPanel.java
@@ -1,5 +1,20 @@
 package com.LuDik.EvoAI;
 
-public class ActionPanel {
+import java.awt.Button;
 
+import javax.swing.JPanel;
+
+import com.simplemove.Board;
+
+public class ActionPanel extends JPanel {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private CameraPanel cameraPanel;
+	
+	private int APHeight ; // hoogte van camerapanel
+	private final int APWidth  = 200;
+	private Board board;
+	
+	private Button startBtn;
 }
