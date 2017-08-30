2e6c12d18646b8b33ba4cb03ebe6c5314f7634a9
LuukCrooijmans9 <31418705+LuukCrooijmans9@users.noreply.github.com>
2017-08-30 15:21:03 +0200

    voegt initialisatie van de main frame toe

--- MainApp/com/LuDik/EvoAI/EvoAI.java
+++ MainApp/com/LuDik/EvoAI/EvoAI.java
@@ -1,5 +1,42 @@
 package com.LuDik.EvoAI;
 
-public class EvoAI {
+import java.awt.FlowLayout;
 
+import javax.swing.JFrame;
+
+import com.simplemove.Board;
+import com.simplemove.InfoPanel;
+
+public class EvoAI extends JFrame {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EvoAI() {
+		initUI();
+		
+	}
+	
+	private void initUI() {
+		setLayout(new FlowLayout());
+		
+		Board board = new Board();
+		add(board);
+
+		InfoPanel infoPanel = new InfoPanel(board);
+		add(infoPanel);
+		
+				
+		setSize(2000, 2000);
+		
+		setResizable(false);
+		pack();
+					
+		setTitle("EvoAI");
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+	}
+	
 }
