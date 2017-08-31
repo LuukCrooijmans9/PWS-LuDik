ce83eace637b3e61037f3c562728c8e115c66ce8
dikva <dikva@DESKTOP-J8R2N4P.home>
2017-08-31 20:43:18 +0200

    test

--- MainApp/com/LuDik/EvoAI/LandTile.java
+++ MainApp/com/LuDik/EvoAI/LandTile.java
@@ -4,11 +4,6 @@ public class LandTile extends Tile {
 	float fertility;
 	float foodValue;
 	
-	public static void calculateNextFood(GlobalFertilityBonus) {
-		globalFertilityBonus = this.globalFertilityBonus
-		if(this.foodValue) {
-			this.foodValue = this.foodValue + this.fertility * GlobalFertilityBonus
-		}
-	}
+	
 	
 }
