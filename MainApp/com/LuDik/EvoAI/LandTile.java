e90ec8f606a01e1306c893577308f9ca9650017a
dikva <dikva@DESKTOP-J8R2N4P.home>
2017-08-31 20:45:08 +0200

    bereken voedsel

--- MainApp/com/LuDik/EvoAI/LandTile.java
+++ MainApp/com/LuDik/EvoAI/LandTile.java
@@ -4,6 +4,11 @@ public class LandTile extends Tile {
 	float fertility;
 	float foodValue;
 	
-	
+	public static void calculateNextFood(GlobalFertilityBonus) {
+		globalFertilityBonus = this.globalFertilityBonus
+		if(this.foodValue) {
+			this.foodValue = this.foodValue + this.fertility * GlobalFertilityBonus
+		}
+	}
 	
 }
