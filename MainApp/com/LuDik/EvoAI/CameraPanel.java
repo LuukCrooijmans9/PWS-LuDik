63649aa6e825e37761ef5b19e223df0921b3cc6c
LuukCrooijmans9 <31418705+LuukCrooijmans9@users.noreply.github.com>
2017-09-02 21:35:33 +0200

    camerapanel neemt nu keyboard inputs en mouseinputs aan

--- MainApp/com/LuDik/EvoAI/CameraPanel.java
+++ MainApp/com/LuDik/EvoAI/CameraPanel.java
@@ -4,45 +4,93 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JPanel;
 
 public class CameraPanel extends JPanel {
-	
+
 	private static final int CPWIDTH = 1000;
 	private static final int CPHEIGHT = 1000;
-	
+
 	private EvoAI mainFrame;
-	
+
 	public CameraPanel(EvoAI parent) {
 		mainFrame = parent;
 		setBackground(Color.WHITE);
-		
+
 		setPreferredSize(new Dimension(CPWIDTH, CPHEIGHT));
+		setFocusable(true);
+		addKeyListener(new KeyInputHandler());
+		addMouseListener(new MouseInputHandler());
 	}
-	
+
 	@Override
-	 public void paintComponent(Graphics g) {
+	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
-		
-		
+
 		doDrawing(g);
-		
+
 	}
-	
+
+	boolean test = false;
+
 	private void doDrawing(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
-		
-		if (mainFrame.getBoard() != null){
+
+		if (mainFrame.getBoard() != null) {
 			mainFrame.getBoard().getMap().drawMap(g2d);
 		}
+
 	}
-	
+
+	class MouseInputHandler extends MouseAdapter {
+		@Override
+		public void mousePressed(MouseEvent e) {
+			requestFocusInWindow();
+			System.out.println("mousepressed");
+		}
+		
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			requestFocusInWindow();
+			System.out.println("mouseEntered");
+		}
+	}
+
+	class KeyInputHandler extends KeyAdapter {
+
+		@Override
+		public void keyPressed(KeyEvent e) {
+			int keyCode = e.getKeyCode();
+
+			switch (keyCode) {
+
+			case KeyEvent.VK_LEFT:
+				break;
+
+			case KeyEvent.VK_RIGHT:
+				break;
+
+			case KeyEvent.VK_DOWN:
+				break;
+
+			case KeyEvent.VK_UP:
+				break;
+
+			case KeyEvent.VK_SPACE:
+				break;
+			}
+		}
+	}
+
 	public void update() {
 		repaint();
 	}
-	
 
 	public static int getCPWIDTH() {
 		return CPWIDTH;
@@ -51,6 +99,5 @@ public class CameraPanel extends JPanel {
 	public static int getCPHEIGHT() {
 		return CPHEIGHT;
 	}
-	
-}
 
+}
