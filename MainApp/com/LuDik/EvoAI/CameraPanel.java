25cf46cf84db7c0159553cdc3531209f3e91e175
LuukCrooijmans9 <31418705+LuukCrooijmans9@users.noreply.github.com>
2017-09-02 22:33:55 +0200

    het beeld van de map kan bewogen worden, inzoomen zou ook moeten kunnen

--- MainApp/com/LuDik/EvoAI/CameraPanel.java
+++ MainApp/com/LuDik/EvoAI/CameraPanel.java
@@ -8,17 +8,27 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseWheelEvent;
+import java.awt.geom.AffineTransform;
 
 import javax.swing.JPanel;
 
 public class CameraPanel extends JPanel {
 
+	private static final long serialVersionUID = 1L;
+	
 	private static final int CPWIDTH = 1000;
 	private static final int CPHEIGHT = 1000;
 
 	private EvoAI mainFrame;
 
+	private double translationX;
+	private double translationY;
+	private double scale = 1;
+	
+	private static final double SCROLL_SPEED = 10;
+	private static final double ZOOM_SPEED = 0.1;
+	
 	public CameraPanel(EvoAI parent) {
 		mainFrame = parent;
 		setBackground(Color.WHITE);
@@ -27,6 +37,7 @@ public class CameraPanel extends JPanel {
 		setFocusable(true);
 		addKeyListener(new KeyInputHandler());
 		addMouseListener(new MouseInputHandler());
+		
 	}
 
 	@Override
@@ -37,17 +48,18 @@ public class CameraPanel extends JPanel {
 
 	}
 
-	boolean test = false;
-
 	private void doDrawing(Graphics g) {
 		Graphics2D g2d = (Graphics2D) g;
+		g2d.translate(translationX, translationY);
+		g2d.scale(scale, scale);
 
 		if (mainFrame.getBoard() != null) {
 			mainFrame.getBoard().getMap().drawMap(g2d);
 		}
 
 	}
-
+	
+	
 	class MouseInputHandler extends MouseAdapter {
 		@Override
 		public void mousePressed(MouseEvent e) {
@@ -60,6 +72,17 @@ public class CameraPanel extends JPanel {
 			requestFocusInWindow();
 			System.out.println("mouseEntered");
 		}
+		
+		@Override
+		public void mouseWheelMoved(MouseWheelEvent e) {
+			System.out.println("mouseScrolled");
+			if (e.getWheelRotation() > 0) {
+				scale += ZOOM_SPEED;
+			} else {
+				scale -= ZOOM_SPEED;
+			}
+			repaint();
+		}
 	}
 
 	class KeyInputHandler extends KeyAdapter {
@@ -71,18 +94,23 @@ public class CameraPanel extends JPanel {
 			switch (keyCode) {
 
 			case KeyEvent.VK_LEFT:
+				translationX += SCROLL_SPEED;
+				repaint();
 				break;
 
 			case KeyEvent.VK_RIGHT:
+				translationX -= SCROLL_SPEED;
+				repaint();
 				break;
 
 			case KeyEvent.VK_DOWN:
+				translationY -= SCROLL_SPEED;
+				repaint();
 				break;
 
 			case KeyEvent.VK_UP:
-				break;
-
-			case KeyEvent.VK_SPACE:
+				translationY += SCROLL_SPEED;
+				repaint();
 				break;
 			}
 		}
