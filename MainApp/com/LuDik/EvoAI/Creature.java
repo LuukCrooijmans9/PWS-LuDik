3dd0b2d433d794fecbb34f8af98f7d3a93eb2e92
LuukCrooijmans9 <31418705+LuukCrooijmans9@users.noreply.github.com>
2017-09-04 22:32:01 +0200

    variables verbetert en constructor toegevoegd

--- MainApp/com/LuDik/EvoAI/Creature.java
+++ MainApp/com/LuDik/EvoAI/Creature.java
@@ -5,27 +5,31 @@ import java.awt.Graphics2D;
 
 public class Creature {
 
-	private long CreatureID; // ID om creature aan te herkennen
+	private long creatureID; // ID om creature aan te herkennen
 	private double age;
-	private long ParentAID, ParentBID; // ID parents
+	private long parentAID, parentBID; // ID parents
 	private double fat, weight; // Voedsel vooraad
-	private double Xpos, Ypos, Direction; // Positie en draaing
+	private double xPos, yPos, Direction; // Positie en draaing
 	private double CreatureSize;
-	private int Xtile, Ytile;
+	private int xTile, yTile;
 	private double speed, maxSpeed;
-	private double DesiredFood, DesiredDrink, foodInMouth, waterInMouth;
+	private double desiredFood, desiredDrink, foodInMouth, waterInMouth;
 
 	Color creatureColor;
 
-	private void Born(long ParentA, long ParentB) {
+	public Creature {
+		
+	}
+	
+	private void born(long parentA, long parentB) {
 		age = 0;
 		fat = 10;
 	}
 
-	private void Consume() {
-		DesiredFood = 1d; // later door brain bepaalt 1 is max 0 is min negatief
-		Xtile = (int) Math.round((Xpos / Configuration.tileSize) + 0.5d);
-		Ytile = (int) Math.round((Ypos / Configuration.tileSize) + 0.5d);
+	private void consume() {
+		desiredFood = 1d; // later door brain bepaalt 1 is max 0 is min negatief
+		xTile = (int) Math.round((xPos / Configuration.tileSize) + 0.5d);
+		yTile = (int) Math.round((yPos / Configuration.tileSize) + 0.5d);
 		
 
 	}
