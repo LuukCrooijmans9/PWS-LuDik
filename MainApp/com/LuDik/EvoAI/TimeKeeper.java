ef2e426160d5dcf3298a48bec4ab4428e195c334
LuukCrooijmans9 <31418705+LuukCrooijmans9@users.noreply.github.com>
2017-09-09 19:04:47 +0200

    basis structuur TimeKeeper aangemaakt

--- MainApp/com/LuDik/EvoAI/TimeKeeper.java
+++ MainApp/com/LuDik/EvoAI/TimeKeeper.java
@@ -1,5 +1,67 @@
 package com.LuDik.EvoAI;
 
-public class TimeKeeper {
+public class TimeKeeper implements Runnable {
+	
+	private Board board;
+	private Thread timeKeeper;
+	private long step;
+	private long DELAY;
+	private boolean paused;
+	
+	
+	public TimeKeeper(Board brd) {
+		board = brd;
+		timeKeeper = new Thread(this);
+	}
+	
+	public void start() {
+		paused = false;
+		timeKeeper.start();
+	}
 
+	private void makeStep() {
+		
+	}
+	
+	@Override
+	public void run() {
+		
+		while (true) {
+			
+			long beforeTime, timeDiff, sleep;
+			
+			
+			
+			beforeTime = System.nanoTime();
+
+			while (true) {
+
+				makeStep();
+				
+				
+				
+				timeDiff = System.nanoTime() - beforeTime;
+				sleep = DELAY - timeDiff;
+				
+				if (sleep < 0) {
+					sleep = 0;
+				}
+
+				try {
+					Thread.sleep(sleep);
+				} catch (InterruptedException e) {
+					System.out.println("Interrupted: " + e.getMessage());
+				}
+				step++;
+								
+				beforeTime = System.nanoTime();
+			}
+			
+			
+		}
+		
+		
+	}
+	
 }
+
