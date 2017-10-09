137c2ceeb0fcb32faec82f2ffc40ffa1197ebd31
LuukCrooijmans9 <31418705+LuukCrooijmans9@users.noreply.github.com>
2017-10-09 18:41:04 +0200

    Main class commented

--- MainApp/com/LuDik/EvoAI/Main.java
+++ MainApp/com/LuDik/EvoAI/Main.java
@@ -2,6 +2,10 @@ package com.LuDik.EvoAI;
 
 import javax.swing.SwingUtilities;
 
+/**
+ * This class contains the main method that runs the EvoAI class.
+ */
+
 public class Main {
 	
 	public static void main(String[] args) {
