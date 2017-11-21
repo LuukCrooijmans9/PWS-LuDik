8c956877591af7290ccfb4e793a1a2c4c8f8e455
LuukCrooijmans9 <31418705+LuukCrooijmans9@users.noreply.github.com>
2017-11-21 18:56:06 +0100

    removed double information for configuration

--- MainApp/com/LuDik/EvoAI/Configuration.java
+++ MainApp/com/LuDik/EvoAI/Configuration.java
@@ -2,52 +2,6 @@ package com.LuDik.EvoAI;
 
 public class Configuration {
 
-	// Map generation
-	public static double mapSmoothness = 0.1; // hoe hoger hoe ruwer de overgang van fertility/water, eigenlijk hoe
-													// ver je ingezoomt zit op de perlin noise.
-	public static  long mainSeed = 0; // Seed is een double van 0 tot 255. negatief laten voor random seed
-	public static  long mapGenSeed = 0; // Seed is een double van 0 tot 255. negatief laten voor random seed
-
-	public static int tileSize = 40;
-	public static  int mapSizeInTiles = 100;
-
-	public static double evolutionFactor = 0.002; // 1 is maximale evolution 0 is geen evolutie
-	
-	public static int beginAmountCreatures = 400;
-	public static int ratioChildsPerParent = 2; // 2 wil zeggen dat elke parent 2 childs krijgt
-	public static int randomCreaturesPerGeneration = 5; // hoeveel volledig nieuwe creatures er bij komen
-																		// per generatie
-
-	// globalFertility bepaalt
-	public static double fertilityMultiplier = 0.001;
-	public static double maxWater = 100;
-	public static double maxFood = 10;
-	public static boolean needDrinking = false;
-
-	// Waarde van creature
-	public static double crtrSize = 10;
-	public static double maxFoodInMouth = 10;
-	public static double maxWaterInMouth = 5;
-	public static double eyeLength = 35;
-	public static double eyeDeviation = 35;
-	public static double eatEfficiencySteepness = 200;
-	public static double startingFat = 10;
-	public static double startingWater = 10;
-	public static double weightPerFat = 0.001;
-	public static double weightPerWater = 0.001;
-	public static double baseFatConsumption = 0.1;
-	public static double baseWaterConsumption = 0.5;
-	public static double maxSpeed = 5;
-	public static double maxDeltaDirection = 10;
-	public static double agePerStep = 0.0001;
-	public static int brainWidth = 3;
-	public static int inputLayerHeight = 13;
-	public static int hiddenLayerHeight = 13;
-
-	
-	
-	
-
 	// een aantal rekenkundige functies.
 
 	// geeft een nummer tussen de min en max waarde. als distribution 1 is wordt
